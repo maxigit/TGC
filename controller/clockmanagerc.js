56d1b0f2734eba84ee26b89c12a94a034c1503b7
Maxime Bourget <mb14@sanger.ac.uk>
2009-11-29 12:28:39 +0000

    Works with padded integer :)

--- controller/clockmanagerc.js
+++ controller/clockmanagerc.js
@@ -57,3 +57,4 @@ function update_clock_controllers()
 }
 
 
+
--- controller/playerclockc.js
+++ controller/playerclockc.js
@@ -5,17 +5,63 @@ function PlayerClockController(clock, clock_div)
 	this.div = clock_div;
 
 	this.remaining_time_label=  this.div.getElementsByClassName('remaining_time')[0];
+	this.remaining_dec_label=  this.div.getElementsByClassName('remaining_dec')[0];
 	this.move_label=  this.div.getElementsByClassName('move')[0];
 
 	//init
 
 	this.update = function()
 	{
-		var remaining_time = this.clock.displayed_remaining_time();
+		var remaining_time = splitTime( this.clock.displayed_remaining_time());
 
-		this.remaining_time_label.textContent= remaining_time;
+		this.remaining_time_label.textContent= pad_int(remaining_time.hour, 2)+":"+pad_int(remaining_time.sec, 2) ;
 
-		this.move_label.textContent = this.clock.move_number;
+		if(this.remaining_dec_label)
+		{
+		this.remaining_dec_label.textContent= remaining_time.dec;
+		}
+
+		this.move_label.textContent = pad_int( this.clock.move_number, 2);
 
 	};
+}
+
+function splitTime(time)
+{
+	var a = new Object();
+	a.milli = time % 10;
+	time -= a.milli;
+	time /= 10;
+
+
+	a.cent = time % 10;
+	time -= a.cent;
+	time /= 10;
+
+	a.dec =time % 10;
+	time -= a.dec;
+	time /= 10;
+
+	a.sec = time % 60;
+	time -= a.sec;
+	time /= 60;
+
+	a.hour = time % 60;
+
+	return a;
+
+}
+
+function pad_left(s, width, pad )
+{
+	while (s.length < width)
+	{
+		s = pad + s;
+	}
+	return s;
+}
+
+function pad_int(a, width)
+{
+	return pad_left(""+a, width, "0");
 }
