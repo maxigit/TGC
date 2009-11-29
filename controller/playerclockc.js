81ea67d4ad159ea73a2efff9c0787c8d1d2cbd25
Maxime Bourget <mb14@sanger.ac.uk>
2009-11-29 13:08:00 +0000

    With Asci handle for 1/4 of second.

--- controller/playerclockc.js
+++ controller/playerclockc.js
@@ -6,6 +6,7 @@ function PlayerClockController(clock, clock_div)
 
 	this.remaining_time_label=  this.div.getElementsByClassName('remaining_time')[0];
 	this.remaining_dec_label=  this.div.getElementsByClassName('remaining_dec')[0];
+	this.wheel_label=  this.div.getElementsByClassName('wheel')[0];
 	this.move_label=  this.div.getElementsByClassName('move')[0];
 
 	//init
@@ -16,9 +17,33 @@ function PlayerClockController(clock, clock_div)
 
 		this.remaining_time_label.textContent= pad_int(remaining_time.hour, 2)+":"+pad_int(remaining_time.sec, 2) ;
 
+
+
 		if(this.remaining_dec_label)
 		{
-		this.remaining_dec_label.textContent= remaining_time.dec;
+			this.remaining_dec_label.textContent= remaining_time.dec;
+		}
+
+		if(this.wheel_label)
+		{
+			var cent = remaining_time.dec*10+remaining_time.cent;
+			cent = (100-cent-5)%100;
+			var wheelchar = '|';
+			if(cent > 75) {
+				wheelchar = '\\';
+			}
+			else if (cent > 50)
+			{
+				wheelchar = '--';
+			}
+			else if (cent > 25)
+			{
+				wheelchar = '/';
+			}
+
+			//wheelchar = Math.floor((100-cent)/25)+1+"";
+
+			this.wheel_label.textContent = wheelchar;
 		}
 
 		this.move_label.textContent = pad_int( this.clock.move_number, 2);
